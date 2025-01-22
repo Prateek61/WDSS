361fda9c759d81f325811b9567452018fdb9ceb6
kiyotone <71599237+kiyotone@users.noreply.github.com>
2025-01-22 20:45:26 +0545

    temporal masking and temporal gbuffers in dataset added

--- config.py
+++ config.py
@@ -6,8 +6,7 @@ class Settings:
     system_log_file = "system.log"
     system_log_to_console = True
     log_dir: str = "logs"
-    dataset_path: str = "D:\\Dev\\MinorProjDataset\\V3\\DATA\\Data\\train"
-    test_dataset_path: str = "D:\\Dev\\MinorProjDataset\\V3\\DATA\\Data\\test"
+    dataset_path = r"D:\Datasets\DataV2\Data"
     system_load_model: bool = False
     system_model_path: str = "models"
     system_model_name: str = "model.pth"
