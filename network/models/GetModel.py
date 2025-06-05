e9550fd68b9f75c0502b1cad6b42d87edd1e61a4
pr-a-sh-ant <prashantaryal07@gmail.com>
2025-06-06 00:07:22 +0545

    small change to my mistake

--- network/models/GetModel.py
+++ network/models/GetModel.py
@@ -1,11 +1,14 @@
 from .WDSSRegular import WDSSRegular, ModelBase
 from .WDSSNoTemp import WDSSNoTemp
+from .WDSSNoWavelet import WDSSNoWavelet
 
 from typing import Dict, Any
 
 def get_model(model_conf: Dict[str, Any]) -> ModelBase:
     if model_conf['name'] == 'WDSSRegular' and model_conf['version'] == 1.0:
         return WDSSRegular()
+    if model_conf['name'] == 'WDSSNoWavelet' and model_conf['version'] == 1.0:
+        return WDSSNoWavelet()
     if model_conf['name'] == 'WDSSNoTemp' and model_conf['version'] == 1.0:
         return WDSSNoTemp()
     
